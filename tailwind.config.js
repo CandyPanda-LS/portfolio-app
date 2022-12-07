8b97bf91aac289c81ebaa1310708f48a58c45b83
Lasal Sandeepa Hettiarachchi <62637475+CandyPanda-LS@users.noreply.github.com>
2022-12-08 02:14:55 +0530

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        burtons: "burtons",
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
